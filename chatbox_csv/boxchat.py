88b2eeddd3dd68f187ba2fb780927a57bd54687c
duan <truongduan333@gmail.com>
2023-08-25 14:22:56 +0700

    bug2

--- chatbox_csv/boxchat.py
+++ chatbox_csv/boxchat.py
@@ -5,27 +5,28 @@ reader = csv.reader(f)
 
 
 data = {}
-isSuccess = False
 
 name = input("BOT : What's your name ? \n")
 print("BOT : hello", name)
 print("----------------")
 
 # read file and loop => save into dictionary
-'''
-  two 2 situation : 
-   situation 1 : 
-     - nếu đọc trực tiếp bằng file nó sẽ đọc đủ số lượng chiều dài của file 
-         ,và không quay lại từ đầu đọc lại đc
+# '''
+#   two 2 situation :
+#    situation 1 :
+#      - nếu đọc trực tiếp bằng file nó sẽ đọc đủ số lượng chiều dài của file
+#          ,và không quay lại từ đầu đọc lại đc
 
-   situation 2: 
-    - lưu data vào 1 dict thì nó sẽ có thể lặp hết vòng và có thể bắt đầu lại được
-'''
+#    situation 2:
+#     - lưu data vào 1 dict thì nó sẽ có thể lặp hết vòng và có thể bắt đầu lại được
+# '''
 for question, answer in reader:
     data[question] = answer
 
 
 while True:
+
+    isSuccess = False
 
     print("BOT : What information do you want to know  ?  ")
     question_from_user = input(f"{name} : ")
@@ -38,10 +39,8 @@ while True:
             print("BOT :", result, "\n")
             isSuccess = True
 
-        if isSuccess == False:
-            print("BOT : Sorry , i don't understand your question ! \n")
-            break
-
+    if isSuccess == False:
+        print("BOT : Sorry , i don't understand your question ! \n")
     #  exit program
     print("BOT : Do you want to ask other questions ? : (yes , no) ")
     pause = input()
