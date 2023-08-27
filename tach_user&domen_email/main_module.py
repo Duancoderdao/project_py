3bc02e386cbd89571b68e0b22f00d235ea6ec844
duan <truongduan333@gmail.com>
2023-08-27 09:57:07 +0700

    lan2

--- tach_user&domen_email/main_module.py
+++ tach_user&domen_email/main_module.py
@@ -1,13 +1,21 @@
-from code_process import email_process, output
+from code_process import email_process
 
 
 def main():
-    emails = ["ayeume@gmail.com",
-              "truognduan999@gmail.vn", "quengochai@gmail.edu"]
-    for email in emails:
-        print(email)
-        user, domain = email_process(email)
-        output(user, domain)
+
+    f = open("D:\coder\DATA_ANALYST\PYTHON\THUC_HANH\\tach_user&domen_email\input.txt",
+             "r", encoding="UTF-8")
+    w = open("D:\coder\DATA_ANALYST\PYTHON\THUC_HANH\\tach_user&domen_email\output.csv",
+             "w", encoding="UTF-8")
+    read_text = f.readlines()
+
+    header_new = "User_name" + "," + "domain_name" + "\n"
+    w.write(header_new)
+
+    for i in read_text:
+        user, domain = email_process(i)
+        row_new = user.strip() + "," + domain.strip() + "\n"
+        w.write(row_new)
 
 
 if __name__ == "__main__":
---
+++ tach_user&domen_email/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+   # w = open("D:\coder\DATA_ANALYST\PYTHON\THUC_HANH\\tach_user&domen_email\output.csv",
+    #          "r", encoding="UTF-8")
